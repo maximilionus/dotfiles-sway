24c15e4d1af08217202923827fe6fd982988d749
maximilionus <maximilionuss@gmail.com>
2025-05-07 16:43:29 +0300

    scripts: Add basic gtk theme switcher

---
+++ scripts/.local/bin/set-system-theme.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set_gsettings_theme () {
+    mode="$1"
+    color_scheme="default"
+    gtk_theme="Adwaita"
+
+    if [[ $mode == "dark" ]]; then
+        color_scheme="prefer-dark"
+        gtk_theme="${gtk_theme}-dark"
+    fi
+
+    gsettings set org.gnome.desktop.interface color-scheme "$color_scheme"
+    gsettings set org.gnome.desktop.interface gtk-theme "$gtk_theme"
+}
+
+
+case $1 in
+    light|0)
+        set_gsettings_theme light
+        ;;
+    dark|1)
+        set_gsettings_theme dark
+        ;;
+esac
