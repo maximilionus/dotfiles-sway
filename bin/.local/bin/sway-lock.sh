f5e046a102935e1c8f5e7f6ce29f62d10a0ba47d
maximilionus <maximilionuss@gmail.com>
2025-06-13 02:43:54 +0300

    bin: Bump monitor poweroff timeout to 10 secs

--- bin/.local/bin/sway-lock.sh
+++ bin/.local/bin/sway-lock.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+POWER_TIMEOUT=10
+
+
 swayidle \
-    timeout 5 'swaymsg "output * power off"' \
+    timeout $POWER_TIMEOUT 'swaymsg "output * power off"' \
     resume 'swaymsg "output * power on"' &
 
 swaylock -c 000000 -s fill -i ~/Pictures/wallpaper
