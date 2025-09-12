b929604d2ed177e6867281739a577275a4d34084
maximilionus <maximilionuss@gmail.com>
2025-09-13 01:19:39 +0300

    swaybar: Backlight module, isolate modules blocks

--- sway/.local/bin/swaybar-status-command.sh
+++ sway/.local/bin/swaybar-status-command.sh
@@ -2,6 +2,10 @@
 KB_ICON="   "
 VOLUME_ACTIVE_ICON="    "
 VOLUME_MUTED_ICON=""
+BATTERY_FULL_ICON="󰁹"
+BATTERY_DISCHARGE_ICON="󰂁"
+BATTERY_CHARGE_ICON="󱐋"
+BACKLIGHT_ICON="󰃠"
 NET_LAN_ICON="󰌗"
 NET_WIFI_ICON="󰖩"
 NET_DOWN_ICON="󰀝"
@@ -21,81 +25,110 @@ keyboard_module=" $KB_ICON$(swaymsg -t get_inputs \
     | cut -c1-2)"
 
 # Audio
-audio_pipewire=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
-audio_module=$(echo "$audio_pipewire" | awk '{print $2*100}')
-audio_module=$(echo "$audio_pipewire" | grep -q MUTED \
-    && echo "$VOLUME_MUTED_ICON" || echo "$VOLUME_ACTIVE_ICON$audio_module%")
+audio_module=""
+
+{
+    audio_pipewire=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
+    audio_module=$(echo "$audio_pipewire" | awk '{print $2*100}')
+    audio_module=$(echo "$audio_pipewire" | grep -q MUTED \
+        && echo "$VOLUME_MUTED_ICON" || echo "$VOLUME_ACTIVE_ICON$audio_module%")
+}
 
 # Battery
 battery_module=""
-battery_status=$(ls /sys/class/power_supply/ | grep BAT | head -n1)
 
-if [[ ! -z $battery_status ]]; then
-    capacity=$(cat /sys/class/power_supply/$battery_status/capacity)
-    status=$(cat /sys/class/power_supply/$battery_status/status)
+{
+    battery_status=$(ls /sys/class/power_supply/ | grep BAT | head -n1)
 
-    if [[ $status = "Charging" ]]; then
-        icon="󱐋"
-    elif [[ $status = "Discharging" ]]; then
-        icon="󰂁"
-    else
-        icon="󰁹"
+    if [[ ! -z $battery_status ]]; then
+        capacity=$(cat /sys/class/power_supply/$battery_status/capacity)
+        status=$(cat /sys/class/power_supply/$battery_status/status)
+
+        if [[ $status = "Charging" ]]; then
+            icon="$BATTERY_CHARGE_ICON"
+        elif [[ $status = "Discharging" ]]; then
+            icon="$BATTERY_DISCHARGE_ICON"
+        else
+            icon="$BATTERY_FULL_ICON"
+        fi
+
+        battery_module="$icon $capacity%"
     fi
+}
 
-    battery_module="$icon $capacity%"
-fi
+# Screen backlight
+backlight_module=""
+
+{
+    backlight_dir="/sys/class/backlight"
+    device=$(ls "$backlight_dir" 2>/dev/null | head -n1)
+
+    if [[ -n "$device" ]]; then
+        max_brightness=$(cat "$backlight_dir/$device/max_brightness")
+        cur_brightness=$(cat "$backlight_dir/$device/brightness")
+        percent=$(( cur_brightness * 100 / max_brightness ))
+        backlight_module="$BACKLIGHT_ICON  $percent%"
+    fi
+}
 
 # Network
 network_module="$NET_DOWN_ICON"
 
-default_iface=$(ip route 2>/dev/null | awk '/^default/ {print $5; exit}')
-if [ -n "$default_iface" ]; then
-    if [[ $(cat /sys/class/net/"$default_iface"/type) -eq 1 ]]; then
-        if [[ $default_iface == wl* ]]; then
-            network_module="$NET_WIFI_ICON"
-        else
-            network_module="$NET_LAN_ICON"
+{
+    default_iface=$(ip route 2>/dev/null | awk '/^default/ {print $5; exit}')
+    if [ -n "$default_iface" ]; then
+        if [[ $(cat /sys/class/net/"$default_iface"/type) -eq 1 ]]; then
+            if [[ $default_iface == wl* ]]; then
+                network_module="$NET_WIFI_ICON"
+            else
+                network_module="$NET_LAN_ICON"
+            fi
         fi
     fi
-fi
+}
 
 # Bluetooth
 bluetooth_module=""
 
-bluetooth_power=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
-if [ "$bluetooth_power" = "yes" ]; then
-    connected=$(bluetoothctl info | grep "Connected: yes")
+{
+    bluetooth_power=$(bluetoothctl show | grep "Powered" | awk '{print $2}')
+    if [ "$bluetooth_power" = "yes" ]; then
+        connected=$(bluetoothctl info | grep "Connected: yes")
 
-    if [ -n "$connected" ]; then
-        bluetooth_module="${BLUETOOTH_ICON}${BLUETOOTH_CONNECTED_ICON}"
-    else
-        bluetooth_module="$BLUETOOTH_ICON"
+        if [ -n "$connected" ]; then
+            bluetooth_module="${BLUETOOTH_ICON}${BLUETOOTH_CONNECTED_ICON}"
+        else
+            bluetooth_module="$BLUETOOTH_ICON"
+        fi
     fi
-fi
+}
 
 # Notifications
 notifications_module=""
 
-if pidof dunst > /dev/null; then
-    paused=$(dunstctl is-paused)
-    count=$(dunstctl count waiting)
+{
+    if pidof dunst > /dev/null; then
+        paused=$(dunstctl is-paused)
+        count=$(dunstctl count waiting)
 
-    if [ "$paused" == "false" ]; then
-        notifications_module="$NOTIFICATIONS_ACTIVE_ICON"
-    else
-        if [ "$count" != "0" ]; then
-            notifications_module="$NOTIFICATIONS_MUTED_ICON <sup>$count</sup>"
+        if [ "$paused" == "false" ]; then
+            notifications_module="$NOTIFICATIONS_ACTIVE_ICON"
         else
-            notifications_module="$NOTIFICATIONS_MUTED_ICON"
+            if [ "$count" != "0" ]; then
+                notifications_module="$NOTIFICATIONS_MUTED_ICON <sup>$count</sup>"
+            else
+                notifications_module="$NOTIFICATIONS_MUTED_ICON"
+            fi
         fi
     fi
-fi
+}
 
 # Formatted final output with proper margin
 # Margin... using spaces. Sorry not sorry :)
 modules=()
 
-[[ -n "$battery_module" ]]       && modules+=(" $battery_module")
+[[ -n "$backlight_module" ]]     && modules+=("  $backlight_module")
+[[ -n "$battery_module" ]]       && modules+=("  $battery_module")
 [[ -n "$bluetooth_module" ]]     && modules+=("  $bluetooth_module")
 [[ -n "$network_module" ]]       && modules+=("  $network_module")
 [[ -n "$audio_module" ]]         && modules+=("  $audio_module")
