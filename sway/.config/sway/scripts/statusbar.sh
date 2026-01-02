d898c3b4214871b6ebca458112a9587a6c9f015f
maximilionus <maximilionuss@gmail.com>
2026-01-03 01:32:25 +0300

    sway: Rearrange statusbar modules

--- sway/.config/sway/scripts/statusbar.sh
+++ sway/.config/sway/scripts/statusbar.sh
@@ -164,13 +164,13 @@ wakelock_module_fnc
 # Margin... using spaces. Sorry not sorry :)
 modules=()
 
+[[ -n "$wakelock_module" ]]      && modules+=("  $SPLITTER  $wakelock_module")
+[[ -n "$notifications_module" ]] && modules+=("  $SPLITTER  $notifications_module")
 [[ -n "$backlight_module" ]]     && modules+=("  $SPLITTER  $backlight_module")
 [[ -n "$battery_module" ]]       && modules+=("  $SPLITTER  $battery_module")
 [[ -n "$bluetooth_module" ]]     && modules+=("  $SPLITTER  $bluetooth_module")
 [[ -n "$network_module" ]]       && modules+=("  $SPLITTER  $network_module")
 [[ -n "$audio_module" ]]         && modules+=("  $SPLITTER  $audio_module")
-[[ -n "$wakelock_module" ]]      && modules+=("  $SPLITTER  $wakelock_module")
-[[ -n "$notifications_module" ]] && modules+=("  $SPLITTER  $notifications_module")
 [[ -n "$keyboard_module" ]]      && modules+=("  $SPLITTER  $keyboard_module")
 [[ -n "$date_module" ]]          && modules+=("  $SPLITTER  $date_module")
 
