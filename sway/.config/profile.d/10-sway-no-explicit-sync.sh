fe812f0872b48413ceb6e76b9a76b54c0e559814
maximilionus <maximilionuss@gmail.com>
2026-01-14 00:37:49 +0300

    sway/profile.d: WLR no explicit sync for nvidia

--- sway/.config/profile.d/10-sway-no-explicit-sync.sh
+++ sway/.config/profile.d/10-sway-no-explicit-sync.sh
@@ -1,1 +1,7 @@
-export WLR_RENDER_NO_EXPLICIT_SYNC=1
+# Disable WLR explicit sync for proprietary nvidia drivers.
+#
+# Fixes the screen flickering issues that occur on wlroots-based environments
+# after the linux-drm-syncobj-v1 merge.
+if lsmod | grep -q nvidia_drm; then
+    export WLR_RENDER_NO_EXPLICIT_SYNC=1
+fi
