51dd7eb44a9cee0ae6d914086aecc1a6b67d49e0
maximilionus <maximilionuss@gmail.com>
2026-01-13 23:24:01 +0300

    sway/profile.d: Export XDG desktop name

--- sway/.config/profile.d/80-sway-env.sh
+++ sway/.config/profile.d/80-sway-env.sh
@@ -1,3 +1,4 @@
+export XDG_CURRENT_DESKTOP=sway
 export SWAY_CONFIG_DIR="${XDG_CONFIG_HOME:-$HOME/.config}/sway"
 export SWAY_SCRIPTS_DIR="$SWAY_CONFIG_DIR/scripts"
 export SWAY_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/var/run/user/$UID}/sway"
